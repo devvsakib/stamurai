8e16bd91916b433d3d0fe3300420e76fa31e0315
DevvSakib <devvsakib@gmail.com>
2023-07-06 16:57:28 +0600

    website title added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="light">
-      <Head />
+      <Head>
+        <title>Task Manager - </title>
+      </Head>
       <body>
         <Main />
         <NextScript />
