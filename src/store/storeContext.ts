9cc39c2fa0ef40f0242bad8b43609be9cb2a0a80
DevvSakib <devvsakib@gmail.com>
2023-07-06 16:03:43 +0600

    context bug fixed

--- src/store/storeContext.ts
+++ src/store/storeContext.ts
@@ -1,13 +1,14 @@
+import { createContext, useContext } from 'react';
 import TaskStore from './TaskStore';
 
 interface StoreContext {
     taskStore: typeof TaskStore.Type;
 }
 
-export const storeContext = React.createContext<StoreContext | null>(null);
+export const storeContext = createContext<StoreContext | null>(null);
 
 export const useStore = (): StoreContext => {
-    const store = React.useContext(storeContext);
+    const store = useContext(storeContext);
     if (!store) {
         throw new Error('useStore must be used within a StoreProvider.');
     }
